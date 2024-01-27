7f6800c5927c68d4c587d9d8b5b5a013b60b0bda
samuelmoores <samueldmoores@gmail.com>
2024-01-27 11:06:50 -0800

    screen setup

---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pygame
+import math
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+
+WIDTH, HEIGHT = 800, 600
+
+pygame.display.set_caption("3D projection in python")
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
+
+    screen.fill(WHITE)
+
+    pygame.display.update()
+
