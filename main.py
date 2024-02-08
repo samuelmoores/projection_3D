587cc51d4ef6c135fb0587b31cbe3b5673232bca
samuelmoores <samueldmoores@gmail.com>
2024-02-07 17:33:35 -0800

    rotate points around center in z

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 import numpy as np
 import pygame
-import math
+from math import *
 
+# ---------------- Constants ------------------------------------
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
-
 WIDTH, HEIGHT = 800, 600
 
+
+# -------------------- Variables -----------------------------
+points = []
+scale = 100
+circle_pos = [WIDTH/2, HEIGHT/2]
+
+angle = 0
+
+# ---------------------- Matrices ------------------------------
+points.append(np.matrix([-1, -1, 1]))
+points.append(np.matrix([1, -1, 1]))
+points.append(np.matrix([1, 1, 1]))
+points.append(np.matrix([-1, 1, 1]))
+points.append(np.matrix([-1, -1, -1]))
+points.append(np.matrix([1, -1, -1]))
+points.append(np.matrix([1, 1, -1]))
+points.append(np.matrix([-1, 1, -1]))
+
+projection_matrix = np.matrix([
+    [1, 0, 0],
+    [0, 1, 0]
+])
+
+# -------------------------- Begin -------------------------------------
 pygame.display.set_caption("3D projection in python")
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+clock = pygame.time.Clock()
+
 while True:
+    clock.tick(60)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -20,7 +48,24 @@ while True:
                 pygame.quit()
                 exit()
 
+    rotation_matrix_z = np.matrix([
+        [cos(angle), -sin(angle), 0],
+        [sin(angle), cos(angle), 0],
+        [0, 0, 1],
+    ])
+
+    angle += 0.01
+
     screen.fill(WHITE)
+
+    # draw points
+    for point in points:
+        rotated_2D = np.dot(rotation_matrix_z, point.reshape((3, 1)))
+        projected_2D = np.dot(projection_matrix, rotated_2D)
+
+        x = int(projected_2D[0][0] * scale) + circle_pos[0]
+        y = int(projected_2D[1][0] * scale) + circle_pos[1]
+        pygame.draw.circle(screen, BLACK, (x, y), 5)
 
     pygame.display.update()
 
