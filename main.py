5fcd5d3a9226805450fa669e44e19672ce4c2c68
samuelmoores <samueldmoores@gmail.com>
2024-02-07 19:37:26 -0800

    connect the points to make a cube

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import numpy as np
 import pygame
 from math import *
 
+def connect_points(i, j, points, color):
+    pygame.draw.line(screen, color, ((points[i][0]), (points[i][1])), (points[j][0], points[j][1]))
+
 # ---------------- Constants ------------------------------------
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+ORANGE = (255, 127, 0)
+PURPLE = (127, 0, 255)
 BLACK = (0, 0, 0)
 WIDTH, HEIGHT = 800, 600
 
@@ -31,6 +38,9 @@ projection_matrix = np.matrix([
     [0, 1, 0]
 ])
 
+projected_points = [
+    [n, n] for n in range(len(points))
+]
 
 # -------------------------- Begin -------------------------------------
 pygame.display.set_caption("3D projection in python")
@@ -71,6 +81,7 @@ while True:
 
     screen.fill(WHITE)
 
+    i = 0
     # draw points
     for point in points:
         rotated_2D = np.dot(rotation_matrix_z, point.reshape((3, 1)))
@@ -80,7 +91,33 @@ while True:
 
         x = int(projected_2D[0][0] * scale) + circle_pos[0]
         y = int(projected_2D[1][0] * scale) + circle_pos[1]
+
+        projected_points[i] = [x, y]
         pygame.draw.circle(screen, BLACK, (x, y), 5)
+        i += 1
+
+    connect_points(0, 1, projected_points, RED)
+    connect_points(4, 5, projected_points, RED)
+
+    connect_points(0, 4, projected_points, BLUE)
+    connect_points(1, 5, projected_points, BLUE)
+
+
+    connect_points(2, 3, projected_points, GREEN)
+    connect_points(6, 7, projected_points, GREEN)
+
+    connect_points(2, 6, projected_points, ORANGE)
+    connect_points(3, 7, projected_points, ORANGE)
+
+    connect_points(0, 3, projected_points, PURPLE)
+    connect_points(1, 2, projected_points, PURPLE)
+
+    connect_points(4, 7, projected_points, PURPLE)
+    connect_points(5, 6, projected_points, PURPLE)
+
+
+
+
 
     pygame.display.update()
 
